02fde24653998b24c4cc8299e4e3d9fe38cda70b
S-E-Pashin <59841297+S-E-Pashin@users.noreply.github.com>
2023-10-18 00:44:35 +0300

    jsx
    jsx препроцессор, ограничения в нем. Переменные и многострочные элементы.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 
 // const elem = <h2>Hello World!!!</h2>;
-const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!!!!');
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!!!!');
+
+const text = "Hello World in 2023!";
+
+const elem = (
+  <div>
+    <h2>{text}</h2>
+    <input type="text"/>
+    <button></button>
+  </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
