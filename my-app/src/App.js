f4a73daa3069e88d9f05782347796b9d3a0992fe
S-E-Pashin <59841297+S-E-Pashin@users.noreply.github.com>
2023-10-20 18:09:25 +0300

    Изменение под пример

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,20 @@
 import {Component} from "react";
 import './App.css';
 
-const Header = () => {
-  return <h2>Текст первого компонента</h2>
+function WhoAmI ({name, surname, link}) {
+  return (
+    <div>
+      <h1>My name is {name}, surname - {surname}</h1>
+      <a href={link}>My profile</a>
+    </div>
+  )
 }
-
-// const Field = () => {
-//   const holder = 'Enter here';
-//   const styleField = {
-//     width: '300px'
-//   };
-//
-//   return <input
-//           placeholder={holder}
-//           type="text"
-//           style={styleField}/>
-// }
-
-class Field extends Component {
-  render() {
-    const holder = 'Enter hereооо';
-    const styleField = {
-      width: '300px'
-    };
-
-    return <input
-      placeholder={holder}
-      type="text"
-      style={styleField}/>
-  }
-}
-
-
-const Btn = () => {
-  const text = 'Log in';
-  const logged = true;
-
-  // const res = () => {
-  //   return 'Log in';
-  // }
-
-  return <button>{logged ? 'Enter' : text}</button>
-}
-
 
 function App() {
   return (
     <div className="App">
-
-      <Header/>
-      <Field/>
-      <Btn/>
-
+      <WhoAmI name="john" surname="Smith" link="fb.com" />
+      <WhoAmI name="Dart" surname="Wader" link="starDeath.com" />
     </div>
   );
 }
-
-export {Header}
-export default App;
